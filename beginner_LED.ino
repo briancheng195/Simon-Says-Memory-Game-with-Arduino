fdfc6f48e185a876efee518d97b469000f7436ee
Brian Cheng <122734373+briancheng195@users.noreply.github.com>
2024-05-18 23:27:35 +0100

    Add files via upload

---
+++ beginner_LED.ino
@@ -0,0 +1,21 @@
+#include <Beginner_LED.h>
+
+
+// global-scope variable
+dig_LED Cpp_led;
+//Note: use globals sparingly (they are easily misued causing bugs)
+
+void setup()
+{  
+  // initialize as output pin (only needed once).
+  int Cpp_led_pin = 7;
+  Cpp_led.setup_LED(Cpp_led_pin);  
+
+}
+
+void loop()
+{
+  int waiting_ms=1000;
+  Cpp_led.blink(waiting_ms);
+  
+}
